04266d3b423713ff3049ec2a8d3b8074bb8d57e0
Victoriya Zaec <vzaec909@gmail.com>
2026-04-22 15:11:35 +0300

    Creating project

--- app/src/main/java/com/exampl/myfirstgitproject/ui/theme/Theme.kt
+++ app/src/main/java/com/exampl/myfirstgitproject/ui/theme/Theme.kt
@@ -14,13 +14,16 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Green
+
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Green
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +40,7 @@ private val LightColorScheme = lightColorScheme(
 fun MyFirstGitProjectTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
